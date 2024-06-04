a94333f4839864f0e7331b26b0bc74133a20cc7f
kalijerry <729913310@qq.com>
2024-06-04 20:03:07 +0800

    日期2024年6月1日

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// 购物车功能
+const addToCartButtons = document.querySelectorAll('.add-to-cart');
+
+addToCartButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        // 添加到购物车逻辑
+    });
+});
+
+// 其他交互功能
